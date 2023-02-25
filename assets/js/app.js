89bc4c7ab8c0dd10332a722197c1071cabe86632
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-02-25 19:05:31 +0300

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+$(function() {
+
+    var header = $("header"), 
+        introH = $("#intro").innerHeight();
+        scrollOffset = $(window).scrollTop();
+
+
+    /* fixed header */    
+    cheeckScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        cheeckScroll(scrollOffset);
+    });
+
+    function cheeckScroll(scrollOffset) {
+        if(scrollOffset >= introH) {
+            header.addClass("fixed"); 
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /* smooth scroll */
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+            $("html, body").animate({
+                scrollTop: blockOffset
+            }, 500);
+    });
+
+    /*menu nav toggle*/
+
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active"); 
+        $("#nav").toggleClass("active"); 
+    });
+
+
+    /*collapse*/
+
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $(this).data('collapse');
+
+        $(this).toggleClass("active");
+    });
+
+
+    /*slider*/
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
